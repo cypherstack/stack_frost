e059d874d0cc13453d55433622475a92fc8ed34a
julian <julian@cypherstack.com>
2023-08-08 09:14:55 -0600

    strip more unused

--- scripts/macos/build_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-(cd ../../crypto_plugins/flutter_liblelantus/scripts/macos && ./build_all.sh ) &
-(cd ../../crypto_plugins/flutter_libepiccash/scripts/macos && ./build_all.sh )  &
-(cd ../../crypto_plugins/flutter_libmonero/scripts/macos/ && ./build_all.sh  ) &
-
-wait
-echo "Done building"
