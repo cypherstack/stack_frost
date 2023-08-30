f63bc21692835c0ef5e1945a957d15e4b9ccfb06
julian <julian@cypherstack.com>
2023-08-30 14:03:15 -0600

    hide themes options

--- lib/pages/settings_views/global_settings_view/appearance_settings/appearance_settings_view.dart
+++ lib/pages/settings_views/global_settings_view/appearance_settings/appearance_settings_view.dart
@@ -10,8 +10,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:stackfrost/pages/settings_views/global_settings_view/appearance_settings/manage_themes.dart';
-import 'package:stackfrost/pages/settings_views/global_settings_view/appearance_settings/sub_widgets/theme_options_widget.dart';
 import 'package:stackfrost/providers/providers.dart';
 import 'package:stackfrost/themes/stack_colors.dart';
 import 'package:stackfrost/utilities/constants.dart';
@@ -19,7 +17,6 @@ import 'package:stackfrost/utilities/text_styles.dart';
 import 'package:stackfrost/widgets/background.dart';
 import 'package:stackfrost/widgets/custom_buttons/app_bar_icon_button.dart';
 import 'package:stackfrost/widgets/custom_buttons/draggable_switch_button.dart';
-import 'package:stackfrost/widgets/desktop/secondary_button.dart';
 import 'package:stackfrost/widgets/rounded_white_container.dart';
 
 class AppearanceSettingsView extends ConsumerWidget {
@@ -107,48 +104,48 @@ class AppearanceSettingsView extends ConsumerWidget {
                             },
                           ),
                         ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        RoundedWhiteContainer(
-                          child: Column(
-                            children: [
-                              Row(
-                                children: [
-                                  Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        "Choose Theme",
-                                        style: STextStyles.titleBold12(context),
-                                        textAlign: TextAlign.left,
-                                      ),
-                                      const SizedBox(
-                                        height: 12,
-                                      ),
-                                      const Padding(
-                                        padding: EdgeInsets.all(4),
-                                        child: ThemeOptionsWidget(),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(
-                                height: 12,
-                              ),
-                              SecondaryButton(
-                                label: "Add more themes",
-                                onPressed: () {
-                                  Navigator.of(context).pushNamed(
-                                    ManageThemesView.routeName,
-                                  );
-                                },
-                              )
-                            ],
-                          ),
-                        ),
+                        // const SizedBox(
+                        //   height: 10,
+                        // ),
+                        // RoundedWhiteContainer(
+                        //   child: Column(
+                        //     children: [
+                        //       Row(
+                        //         children: [
+                        //           Column(
+                        //             crossAxisAlignment:
+                        //                 CrossAxisAlignment.start,
+                        //             children: [
+                        //               Text(
+                        //                 "Choose Theme",
+                        //                 style: STextStyles.titleBold12(context),
+                        //                 textAlign: TextAlign.left,
+                        //               ),
+                        //               const SizedBox(
+                        //                 height: 12,
+                        //               ),
+                        //               const Padding(
+                        //                 padding: EdgeInsets.all(4),
+                        //                 child: ThemeOptionsWidget(),
+                        //               ),
+                        //             ],
+                        //           ),
+                        //         ],
+                        //       ),
+                        //       const SizedBox(
+                        //         height: 12,
+                        //       ),
+                        //       SecondaryButton(
+                        //         label: "Add more themes",
+                        //         onPressed: () {
+                        //           Navigator.of(context).pushNamed(
+                        //             ManageThemesView.routeName,
+                        //           );
+                        //         },
+                        //       )
+                        //     ],
+                        //   ),
+                        // ),
                       ],
                     ),
                   ),
