dacfe0f5d4cac8b415017a7f67f10ec2c5725b1c
ryleedavis <rylee@cypherstack.com>
2023-08-31 14:17:57 -0600

    show about view

--- lib/pages/settings_views/global_settings_view/global_settings_view.dart
+++ lib/pages/settings_views/global_settings_view/global_settings_view.dart
@@ -11,6 +11,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:stackfrost/pages/settings_views/global_settings_view/about_view.dart';
 import 'package:stackfrost/pages/settings_views/global_settings_view/advanced_views/advanced_settings_view.dart';
 import 'package:stackfrost/pages/settings_views/global_settings_view/delete_account_view.dart';
 import 'package:stackfrost/pages/settings_views/global_settings_view/manage_nodes_views/manage_nodes_view.dart';
@@ -118,18 +119,18 @@ class GlobalSettingsView extends StatelessWidget {
                                           DeleteAccountView.routeName);
                                     },
                                   ),
-                                // const SizedBox(
-                                //   height: 8,
-                                // ),
-                                // SettingsListButton(
-                                //   iconAssetName: Assets.svg.ellipsis,
-                                //   iconSize: 18,
-                                //   title: "About",
-                                //   onPressed: () {
-                                //     Navigator.of(context)
-                                //         .pushNamed(AboutView.routeName);
-                                //   },
-                                // ),
+                                const SizedBox(
+                                  height: 8,
+                                ),
+                                SettingsListButton(
+                                  iconAssetName: Assets.svg.ellipsis,
+                                  iconSize: 18,
+                                  title: "About",
+                                  onPressed: () {
+                                    Navigator.of(context)
+                                        .pushNamed(AboutView.routeName);
+                                  },
+                                ),
                                 const SizedBox(
                                   height: 8,
                                 ),
